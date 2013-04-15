7a587cfa39787e290ad21a5f62aa91443702a760
MrDalang <zhengt.cn@gmail.com>
2013-04-15 11:19:06 +0800

    init

---
+++ ArrayAndDictionary.h
@@ -0,0 +1,37 @@
+//
+//  ArrayAndDictionary.h
+//  notesForObjectiveC
+//
+//  Created by Ibokan on 13-4-15.
+//  Copyright (c) 2013年 zht. All rights reserved.
+//
+
+#ifndef notesForObjectiveC_ArrayAndDictionary_h
+#define notesForObjectiveC_ArrayAndDictionary_h
+
+/* NSArray
+ NSArray* myArray = [NSArray arrayWithObjects:@"obj1", @"obj2", @"obj3", @"obj4", nil];
+ unsigned long length = [myArray count];
+ for (int i = 0; i < length; i++) {
+ //3
+ NSLog(@"the %d obj of MyArray: %@", i, [myArray objectAtIndex:i]);
+ }
+ */
+
+/* NSMutableArray
+ -(void) addObject :  (id) anObject;
+ -(void) removeObjectAtIndex : (unsigned long)index;
+ -(void) insertObject:(id)atIndex: (NSUInteger) index;
+ -(void) replaceObjectAtIndex:(NSUInteger) withObject:(id)
+ */
+
+/* NSEnumerator
+ -(NSEnumerator *) objectEnumerator;
+ NSEnumerator *enumerator = [array objectEnumerator];
+ 
+ id obj;
+ while(obj = [enumerator nextObject]){
+ NSLog(@”%@\n”,obj);
+ }
+ */
+#endif
