f8bf44e331510cfc0896000b9e867b0fc3f75341
MrDalang <zhengt.cn@gmail.com>
2013-04-15 11:12:56 +0800

    Initial Commit

---
+++ notesForObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  notesForObjectiveC
+//
+//  Created by Ibokan on 13-4-15.
+//  Copyright (c) 2013年 zht. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
